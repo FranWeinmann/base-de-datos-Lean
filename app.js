df2bb22e923a51e3fb32443c7cc4c4f79b3cbb26
FranWeinmann <weinmann.fran@gmail.com>
2025-08-29 08:51:03 -0300

    .

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import {config} from './dbconfig.js'
 import express from "express";
 import 'dotenv/config'
+import bcryptp from 'bcrypt';
 
 import pkg from 'pg'
 const {Client} = pkg;
@@ -18,14 +19,32 @@ app.get('/about', (req, res) => {
   res.send('About route 🎉 ')
 })
 
+app.post('/createuser', async (req, res) => {
+  const user = req.body;
+  console.log("User data: ", user);
+  if(!user.nombre || !user.userid || !user.password){
+    return res.status(400).json({message: "Completa todos los campos"});
+  }
+  try {
+    const client = new Client(config);
+    await client.connect();
+    const hashPad = await bcryptp.hash(user.password, 10);
+    user.password = hashPad;
+    let result = await client.query('INSERT INTO usuario VALUES ($1, $2, $3 returning *', [
+      user.userid, user.nombre, user.password
+    ]);
+    await  client.end();
+    console.log('User created: ', result.rowCount);
+  }
+})
+
 app.get('/canciones', async (req, res) => {
   const client = new Client(config);
   await client.connect();
   let result = await client.query("select * from public.canciones");
   await client.end();
   console.log(result.rows);
-  res.send(result.rows)
-
+  res.send(result.rows);
 })
 
 app.listen(PORT, () => {
